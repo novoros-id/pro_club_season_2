b563773dee3fdf8e86f76a9b0be5a2af46d21404
Vaganov Alexey <novoros-id@inbox.ru>
2025-07-06 22:43:02 +0300

    добавлена сборка

---
+++ prep/main.py
@@ -0,0 +1,25 @@
+from prepare_files.prepare_files import prepare_files
+from transcription_audio.transcription import Transcription
+from create_file.create_docx import create_docx
+
+def process_video(video_path):
+    # 1. Подготовка аудиофайлов из видео
+    prep = prepare_files(video_path)
+    files = prep.process_file()
+    audio_file = files['audio']  # Получаем путь к аудиофайлу
+    print(f"[LOG] prepare_files результат: {files}")
+    
+    # 2. Транскрибация аудиофайла
+    transcription = Transcription()
+    transcription_json = transcription.save_json(audio_file)
+    print(f"[LOG] Transcription результат: {transcription_json}")
+    
+    # 3. Создание DOCX из транскрипта
+    class_create_docx = create_docx(transcription_json)
+    paragraph = class_create_docx.get_docx()
+    print(f"[LOG] create_docx результат: {paragraph}")
+
+# Пример использования:
+if __name__ == "__main__":
+    video_path = '/Users/alexeyvaganov/Documents/doc/Работа/КЛУБ РАЗРАБОТЧИКОВ/Инструкция по боту_720.mov'
+    process_video(video_path)
